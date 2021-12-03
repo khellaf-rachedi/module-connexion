1d7973a8412e96f8a0c93a2af6a5cea92c7f18c3
khellaf-rachedi <91748769+khellaf-rachedi@users.noreply.github.com>
2021-12-03 20:00:05 +0100

    ajout page accueil "index"

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+include('bdd.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title> Accueil module de connexion </title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+</head>
+
+<body>
+    <header>
+        <section>
+            <article>
+                <img class="logo-ariane" src="https://zupimages.net/up/21/47/b5rb.png" alt="logo accueil" />
+            </article>
+            <ul class="navbar">
+                <li>
+                    <a href="index.php"><button>Accueil</button></a>
+                </li>
+                <?php if(!(isset($_SESSION['login']))){  #si la session utilisateur n'existe pas on affiche seulement les boutons pour les pages inscription et connexion# 
+                echo"
+                <li>
+                    <a href=\"inscription.php\"><button>Inscription</button></a>
+                </li>
+                <li>
+                    <a href=\"connexion.php\"><button>Connexion</button></a>
+                </li>";
+
+                }
+                else{ #sinon si la session utilisateur existe, que l'utilisateur est connecté, on affiche les boutons profil et modifier profil, et il pourra se déconnecter#
+                    echo"<li>
+                    <a href=\"profil.php?id=".$_SESSION['id']."\"><button>Profil</button></a>
+                </li>
+                <li>
+                <a href=\"editionprofil.php?\"><button>Modifier mon profil</button></a>
+                <li>
+                    <a href=\"deconnexion.php\"><button class=\"bouton-deconnexion\">Deconnexion</button></a>
+                </li>";
+                ;
+                }
+                ?>
+                
+                <li>
+                    <a href="#"><button>Github</button></a>
+                </li>
+            </ul>
+        </section>
+    </header>
+        <h1 class="titre-accueil">Bienvenue sur le Ariane Club</h1>
+        </br>
+        <video width="1500" height="440" autoplay loop muted>
+            <source src="https://www.ariane.group/wp-content/themes/ariane-parent/library/video/newsletter/push_newsletter_ribbon.mp4" type="video/mp4">
+            </br>
+            <div class="flex-images">
+                <img class="fusee" src="https://zupimages.net/up/21/48/txkn.jpg" alt="fusée 1">
+                <img class="fusee" src="https://zupimages.net/up/21/47/4ow6.jpg" alt="fusée 2">
+            </div>
+    <footer>
+        <div class="row">
+            <div>
+                <h3><a href="index.php">ACCUEIL</a></h3>
+            </div>
+            <div>
+                <h3> <a href="">GITHUB</a></h3>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
